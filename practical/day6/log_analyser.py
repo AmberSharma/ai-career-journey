5b35cc48ebc6184eec0149bd014c357dbded8341
Amber Sharma <amber.sharma@gartner.com>
2026-04-04 11:46:34 +0530

    Day 6: Log Analyser

---
+++ practical/day6/log_analyser.py
@@ -0,0 +1,31 @@
+
+def analyse_log(log_file):
+    counts = {
+        "info": 0,
+        "warning": 0,
+        "error": 0
+    }
+    try:
+        with open(log_file) as f:
+            lines = f.readlines()
+
+            for line in lines:
+                if "INFO" in line:
+                    counts["info"] += 1
+                elif "WARNING" in line:
+                    counts["warning"] += 1
+                elif "ERROR" in line:
+                    counts["error"] += 1
+
+            return counts
+    except FileNotFoundError:
+        print("File not found")
+        exit()
+
+result = analyse_log('log.txt')
+print(result)
+total_errors = sum(result.values())
+
+for key, value in result.items():
+    # print(f"{key} %: ", value/total_errors*100)
+    print(f"{key}: {value} ({(value/total_errors)*100:.2f} %)")
