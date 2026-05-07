37a2dbbdc814b0af5600c7ad103d733c8eb1b141
Amber Sharma <amber.sharma@gartner.com>
2026-05-07 15:04:30 +0530

    Day 26: Logger

--- project/log_analyser/day13/routes/queue_routes.py
+++ project/log_analyser/day13/routes/queue_routes.py
@@ -1,14 +1,17 @@
 from fastapi import APIRouter
 from services.queue_service import job_status, submit_job
 from models.log_model import Queue
+from utils.logger import logger
 
 router = APIRouter()
 
 @router.post("/submit")
 def submit(payload: Queue):
+    logger.info("Received request at /submit")
     job_id = submit_job(payload)
     return {"job_id": job_id}
 
 @router.get("/status/{job_id}")
 def status(job_id):
+    logger.info(f"Received request at /status/{job_id}")
     return job_status(job_id)
--- project/log_analyser/day13/services/queue_service.py
+++ project/log_analyser/day13/services/queue_service.py
@@ -2,22 +2,31 @@ from utils.job_store import jobs
 import time
 import threading
 import uuid
+from utils.logger import logger
 
 def process_job(job_id, payload):
+    start = time.time()
     jobs[job_id]["status"] = "processing ₹"
-
+    logger.info(f"Job {job_id} processing started at {time.time() - start}")
     time.sleep(10)
     jobs[job_id]["status"] = "completed"
     jobs[job_id]["result"] = {"message": f"processing payload: {payload}"}
+    duration = time.time() - start
+    logger.info(f"Job {job_id} processing job {payload} took {duration} seconds")
+    logger.info(f"Job {job_id} completed")
+
 
 
 def submit_job(payload):
+    db =
     job_id = str(uuid.uuid4())
 
     jobs[job_id] = {
         "status": "queued",
         "result": None,
     }
+
+    logger.info(f"{job_id} submitting job {payload}")
     threading.Thread(target=process_job, args=(job_id, payload)).start()
 
     return job_id
